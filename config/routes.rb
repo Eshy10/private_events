c8a69354dd1268f33298c7886853a829c25fa397
modupe <adewalebose15@gmail.com>
2020-05-07 15:57:36 +0100

    Add sessions to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+ resources :users, only: [:new, :create, :show]
+ resources :sessions, only: [:new, :create, :destroy]
+ root to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
