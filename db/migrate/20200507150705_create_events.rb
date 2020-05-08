e7775e942954621af21b6afb4a7249051d47d612
modupe <adewalebose15@gmail.com>
2020-05-08 14:15:39 +0100

    add title and location to events table

--- db/migrate/20200507150705_create_events.rb
+++ db/migrate/20200507150705_create_events.rb
@@ -3,6 +3,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.date :date
       t.integer :creator_id
+      t.text :description
+      t.string :location
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_154910) do
+ActiveRecord::Schema.define(version: 2020_05_08_123455) do
 
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.integer "creator_id"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
