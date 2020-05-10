71f1835d9461a76010960bca93c62acc814b84a7
modupe <adewalebose15@gmail.com>
2020-05-10 09:57:48 +0100

    Add attendees to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
+        flash.notice = "Account created succesfully welcome #{@user}"
         log_in @user
-        redirect_to @user
+        redirect_to root_path
       else
         render :new
       end
