39d23741f57207d124cdeb909f6ee3b0f76161d4
Theophile-Kango <fadhili.kango@gmail.com>
2020-05-11 17:01:03 +0200

    Adding test for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        flash.notice = "Account created succesfully welcome #{@user}"
+        flash.notice = "Account created succesfully welcome #{@user.name}"
         log_in @user
         redirect_to root_path
       else
